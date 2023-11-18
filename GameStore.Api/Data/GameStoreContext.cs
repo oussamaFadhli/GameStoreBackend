2fa1b3c940a60c7f793d16bc9a84f6bc4a48bfc1
Oussama Fadhli <www.oussama55aw5@gmail.com>
2023-11-18 15:06:10 +0100

    using the entity framework repository

--- GameStore.Api/Data/GameStoreContext.cs
+++ GameStore.Api/Data/GameStoreContext.cs
@@ -2,19 +2,22 @@ using System.Reflection;
 using GameStore.Api.Entities;
 using Microsoft.EntityFrameworkCore;
 
-namespace GameStore.Api.Data;
-
-public class GameStoreContext : DbContext
+namespace GameStore.Api.Data
 {
-    private DbSet<Game> games;
-
-    public GameStoreContext(DbContextOptions<GameStoreContext> options) : base(options)
+    public class GameStoreContext : DbContext
     {
+        private DbSet<Game> games = null!;
 
-    }
-    public DbSet<Game> Games => Set<Game>();
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        public GameStoreContext(DbContextOptions<GameStoreContext> options) : base(options)
+        {
+            games = Set<Game>();
+        }
+
+        public DbSet<Game> Games => games;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
     }
 }
--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -4,10 +4,13 @@ using GameStore.Api.Repositories;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddSingleton<IGamesRepository, InMemGamesRepository>();
-var connString = builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddSqlServer<GameStoreContext>(connString);
+
+builder.Services.AddRepositories(builder.Configuration);
+
 var app = builder.Build();
+
 app.Services.InitialzeDb();
+
 app.MapGamesEndpoints();
+
 app.Run();
