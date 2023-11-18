b6442aa62b2e6a7e9811b7fab0b176ab7a9a7b2b
Oussama Fadhli <www.oussama55aw5@gmail.com>
2023-11-18 14:44:20 +0100

    add auto migrations and implementing an entity framework repository

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,11 +1,13 @@
 using GameStore.Api.Data;
 using GameStore.Api.Endpoints;
 using GameStore.Api.Repositories;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddSingleton<IGamesRepository, InMemGamesRepository>();
 var connString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddSqlServer<GameStoreContext>(connString);
 var app = builder.Build();
+app.Services.InitialzeDb();
 app.MapGamesEndpoints();
 app.Run();
